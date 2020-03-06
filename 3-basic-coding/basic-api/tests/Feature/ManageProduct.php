bb0a3939d3156b012c987a2fed94c53ae66a9815
Sultan <im.sultaan05@gmail.com>
2020-03-06 18:49:51 +0700

    test crud product

---
+++ 3-basic-coding/basic-api/tests/Feature/ManageProduct.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManageProduct extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function a_user_can_see_all_products()
+    {
+        $this->signIn();
+
+        $product = factory('App\Product')->create([
+            'name' => 'Product 1',
+            'description' => 'Description Product 1'
+        ]);
+        $response = $this->getJson('products')->assertStatus(200);
+
+        $this->assertEquals(1, count($response->json()));
+
+        $this->assertEquals('Product 1', $response->json()['data'][0]['name']);
+    }
+
+    /** @test */
+    public function a_user_can_create_a_product()
+    {
+        // $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $data = factory('App\Product')->raw();
+        $response = $this->postJson('products', $data)
+        ->assertStatus(201)
+        ->assertJsonStructure(['data' => ['id', 'name', 'description', 'price', 'created_at', 'updated_at']]);
+
+        $this->assertEquals(1, count($response->json()));
+    }
+
+    /** @test */
+    public function a_user_can_update_a_product()
+    {
+        $this->signIn();
+
+        $attributes = ['name' => 'product changed', 'price' => 1000, 'description' => 'changed'];
+
+        $product = factory('App\Product')->create();
+
+        $response = $this->putJson("products/$product->id", $attributes)
+        ->assertStatus(200)
+        ->assertJsonStructure(['data' => ['id', 'name', 'description', 'price', 'created_at', 'updated_at']]);
+
+        $this->assertDatabaseHas('products', $attributes);
+
+        $this->assertEquals('product changed', $response->json()['data']['name']);
+    }
+
+    /** @test */
+    public function a_user_can_delete_a_product()
+    {
+        $this->signIn();
+
+        $product = factory('App\Product')->create([
+            'name' => 'Product 1',
+            'description' => 'Description Product 1'
+        ]);
+
+        $this->deleteJson("products/$product->id")->assertStatus(204);
+
+        $this->assertDatabaseMissing('products', $product->only('id'));
+    }
+}
--- 3-basic-coding/basic-api/tests/TestCase.php
+++ 3-basic-coding/basic-api/tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Airlock\Airlock;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signIn($user = null)
+    {
+        $user = $user ?: factory('App\User')->create();
+        Airlock::actingAs(
+            $user,
+            ['*']
+        );
+        return $user;
+    }
 }
